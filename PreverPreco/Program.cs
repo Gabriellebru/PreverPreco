031788cda09ee89c2cb5ed619935365ad9ad25d6
gabri <gabri@DELL-GABRIELLE>
2022-04-10 15:47:19 -0300

    Adicionar arquivos de projeto.

---
+++ PreverPreco/Program.cs
@@ -0,0 +1,33 @@
+﻿using static PreverPreco.PrevisaoDePrecos;
+
+var samples = new List<ModelInput>()
+{
+    new ModelInput()
+    {
+        Bedrooms = 2F,
+        Bathrooms = 1F,
+        Sqft_lot = 10000F,
+        Floors = 2F
+    },
+    new ModelInput()
+    {
+        Bedrooms = 5F,
+        Bathrooms = 2.25F,
+        Sqft_lot = 20158F,
+        Floors = 1F
+    },
+    new ModelInput()
+    {
+        Bedrooms = 6F,
+        Bathrooms = 2.5F,
+        Sqft_lot = 14034F,
+        Floors = 1F
+    },
+};
+
+var outputs = PredictAll(samples);
+foreach (var output in outputs)
+{
+    Console.WriteLine($"Valor sugerido: {output.Score}");
+}
+Console.ReadKey();
---
+++ PrevisaoDePrecos_Console/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace PrevisaoDePrecos_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrevisaoDePrecos.ModelInput sampleData = new PrevisaoDePrecos.ModelInput()
+            {
+                Bedrooms = 3F,
+                Bathrooms = 1F,
+                Sqft_lot = 5650F,
+                Floors = 1F,
+            };
+
+            var predictionResult = PrevisaoDePrecos.Predict(sampleData);
+
+            Console.WriteLine("Using model to make single prediction -- Comparing actual Price with predicted Price from sample data...\n\n");
+
+            Console.WriteLine($"Price: {221900F}");
+            Console.WriteLine($"Bedrooms: {3F}");
+            Console.WriteLine($"Bathrooms: {1F}");
+            Console.WriteLine($"Sqft_lot: {5650F}");
+            Console.WriteLine($"Floors: {1F}");
+
+
+            Console.WriteLine($"\n\nPredicted Price: {predictionResult.Score}\n\n");
+            Console.WriteLine("=============== End of process, hit any key to finish ===============");
+            Console.ReadKey();
+        }
+    }
+}
